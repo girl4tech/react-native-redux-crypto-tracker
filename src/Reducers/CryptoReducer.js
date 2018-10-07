3353b813a0519c9ad2ef1c73aa9431fc66c2dd8e
Karina D <karinad@Karinas-MacBook-Pro.local>
2018-10-07 06:04:28 -0400

    second commit for remote repo

---
+++ src/Reducers/CryptoReducer.js
@@ -0,0 +1,42 @@
+import {
+    FETCHING_COIN_DATA,
+    FETCHING_COIN_DATA_SUCCESS,
+    FETCHING_COIN_DATA_FAIL
+} from './../Utils/ActionTypes';
+
+const initialState = {
+    isFetching: false,
+    data: [],
+    hasError: false,
+    errorMessage: null
+}
+
+export default function(state=initialState, action){
+    switch(action.type){
+        case FETCHING_COIN_DATA:
+            return Object.assign({}, state,{
+                isFetching: true,
+                data:null,
+                hasError: false,
+                errorMessage:null
+            });
+        case FETCHING_COIN_DATA_SUCCESS:
+            return Object.assign({}, state, {
+                isFetching:false,
+                data:action.payload,
+                hasError:false,
+                errorMessage:null
+            });
+        case FETCHING_COIN_DATA_FAIL:
+            return Object.assign({}, state, {
+                isFetching: false,
+                data: action.payload,
+                hasError: true,
+                errorMessage: action.err
+            });
+        
+        default:
+            return state;
+    }
+    
+}
---
+++ src/components/CryptoContainer.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {View, Text, ScrollView, StyleSheet} from 'react-native';
+import Spinner from 'react-native-loading-spinner-overlay';
+
+import FetchCoinData from './../Actions/FetchCoinData';
+import CoinCard from './CoinCard';
+
+class CryptoContainer extends Component{
+
+    componentWillMount(){
+        this.props.FetchCoinData();
+    }
+
+    renderCoinCards(){
+        const {crypto} = this.props;
+        return crypto.data.map((coin)=>
+            <CoinCard
+                key={coin.name}
+                coin_name={coin.name}
+                symbol={coin.symbol}
+                price_usd={coin.price_usd}
+                percent_change_24h={coin.percent_change_24h}
+                percent_change_7d={coin.percent_change_7d}
+            />
+        )
+    }
+
+    render(){
+        const {crypto} = this.props;
+        const {contentContainer} = styles;
+
+        if (crypto.isFetching){
+            return(
+                <View>
+                    <Spinner 
+                        visible={crypto.isFetching}
+                        textContent={"Loading..."}
+                        textStyle={{color:'#253145'}}
+                        animation="fade"
+                    />
+                </View>
+            )
+        }
+
+        return(
+            <ScrollView contentContainerStyle={contentContainer}>
+                {this.renderCoinCards()}
+            </ScrollView>
+        )
+    }
+}
+
+const styles ={
+    contentContainer:{
+        paddingBottom:140,
+        paddingTop:55
+    }
+}
+
+function mapStateToProps(state){
+    return{
+        crypto: state.crypto
+    }
+}
+
+export default connect(mapStateToProps, {FetchCoinData})(CryptoContainer)
